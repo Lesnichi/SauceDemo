5806eaf866ba3b0b287bb35c93151eb27538b096
Uladzislau Liasnichy <51000407+Lesnichi@users.noreply.github.com>
2021-03-18 20:22:56 +0300

    Final task + Allure

--- src/test/java/tests/LogInTest.java
+++ src/test/java/tests/LogInTest.java
@@ -8,19 +8,19 @@ public class LogInTest extends BaseTest {
 
     @Test(retryAnalyzer = Retry.class, description = "Enter wrong password")
     public void wrongPassword() {
-        logInPageFactory.open();
-        logInPageFactory.login(userNameOK, "123");
+        logInPage.open();
+        logInPage.login(userNameOK, "123");
         productPage.isPageOpened3();
-        assertEquals(logInPageFactory.getErrorMassage(),
+        assertEquals(logInPage.getErrorMassage(),
                 "Epic sadface: Username and password do not match any user in this service",
                 "Error massage is not correct");
     }
 
     @Test(retryAnalyzer = Retry.class)
     public void emptyUserName() {
-        logInPageFactory.open();
-        logInPageFactory.login("", "");
-        assertEquals(logInPageFactory.getErrorMassage(),
+        logInPage.open();
+        logInPage.login("", "");
+        assertEquals(logInPage.getErrorMassage(),
                 "Epic sadface: Username is required",
                 "Error massage is not correct");
     }
