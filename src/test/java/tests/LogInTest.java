e8c2f657c1cb3dffb17356d4393d8679cb0e2f3f
Uladzislau Liasnichy <51000407+Lesnichi@users.noreply.github.com>
2021-03-23 19:06:12 +0300

    Final task + Allure

--- src/test/java/tests/LogInTest.java
+++ src/test/java/tests/LogInTest.java
@@ -1,22 +1,29 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
 public class LogInTest extends BaseTest {
 
+    @Test(retryAnalyzer = Retry.class, description = "Enter correct password")
+    public void correctPassword() {
+        logInPage.open();
+        logInPage.login(userNameOK, passwordOK);
+        Assert.assertTrue(productPage.isPageOpened2());
+    }
+
     @Test(retryAnalyzer = Retry.class, description = "Enter wrong password")
     public void wrongPassword() {
         logInPage.open();
         logInPage.login(userNameOK, "123");
-        productPage.isPageOpened3();
         assertEquals(logInPage.getErrorMassage(),
                 "Epic sadface: Username and password do not match any user in this service",
                 "Error massage is not correct");
     }
 
-    @Test(retryAnalyzer = Retry.class)
+    @Test(retryAnalyzer = Retry.class, description = "Enter empty form")
     public void emptyUserName() {
         logInPage.open();
         logInPage.login("", "");
